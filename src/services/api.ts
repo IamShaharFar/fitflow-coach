42f2f1924bc10479381d54102613a2db302227f9
IamShaharFar <shaharfar16@gmail.com>
2025-09-26 14:27:07 +0300

    dev 2 without mocks

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
-import { mockApi } from "./mockService";
+// import { mockApi } from "./mockService";
 import { realApi } from "./realService";
 import type { Api } from "./api.types";
 
 const useMocks = import.meta.env.VITE_USE_MOCKS === "true";
 
-export const api: Api = useMocks ? mockApi : realApi;
+export const api: Api = useMocks ? realApi : realApi;
